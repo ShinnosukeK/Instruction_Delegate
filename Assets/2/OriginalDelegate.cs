7f0f6a9de03bd79eddf105dd03f868593256938f
Shinnosuke Kitayama <k.shinnosuke0603@gmail.com>
2023-07-14 18:11:43 +0900

    Answer2

--- Assets/2/OriginalDelegate.cs
+++ Assets/2/OriginalDelegate.cs
@@ -33,14 +33,21 @@ public class OriginalDelegate : MonoBehaviour
 
             // Step2: Step1のコメントアウトを解除し、
             // 以下の文全体をif文で包み、「もしもcheckerがnullではないなら」実行するようにしてください
-            checker(_hp);
+            /*
+            if(checker != null)
+            {
+                checker(_hp);
+            }
+            */
 
             // Step3: 上に書いたif文は長ったらしいです。それを、null条件演算子?を用いて、
             // checker?.Invoke(_hp)で楽にかけることを確認してください。
+            checker?.Invoke(_hp);
 
             // Step4: そして、Step3の手順は、checker?(_hp)とはかけないことを確認してください。
             // したがって、checker(_hp)という使い方を紹介したものの、
             // 推奨されるのはInvokeを使った、checker?.Invoke(_hp)です。よって、基本Invoke使いましょう。
+            // checker?(_hp); ←これはできない！
         }
 
         ShowHP();
