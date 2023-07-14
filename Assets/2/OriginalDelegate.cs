7e81c54fa59027b5f812baf9c9866e51d5eb9220
Shinnosuke Kitayama <k.shinnosuke0603@gmail.com>
2023-07-14 18:11:13 +0900

    Quiz2【delegateがnullだった場合のエラー処理を追加しよう】

---
+++ Assets/2/OriginalDelegate.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//【テーマ】delegateがnullだった場合の安全処理を追加しよう
+
+public class OriginalDelegate : MonoBehaviour
+{
+    public delegate void Check(int num);
+
+    void CheckDead(int hp)
+    {
+        if (hp > 0)
+        {
+            Debug.Log("生きている");
+        }
+        else
+        {
+            Debug.Log("力尽きた！");
+        }
+    }
+
+    int _hp = 100;//playerのHP
+    [SerializeField] Text _hpText;//HP表示テキスト
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Check checker = null;
+
+            // Step1: CheckDeadを代入している以下の文をコメントアウトするとエラーが出ることを確認してください
+            checker = CheckDead;
+
+            // Step2: Step1のコメントアウトを解除し、
+            // 以下の文全体をif文で包み、「もしもcheckerがnullではないなら」実行するようにしてください
+            checker(_hp);
+
+            // Step3: 上に書いたif文は長ったらしいです。それを、null条件演算子?を用いて、
+            // checker?.Invoke(_hp)で楽にかけることを確認してください。
+
+            // Step4: そして、Step3の手順は、checker?(_hp)とはかけないことを確認してください。
+            // したがって、checker(_hp)という使い方を紹介したものの、
+            // 推奨されるのはInvokeを使った、checker?.Invoke(_hp)です。よって、基本Invoke使いましょう。
+        }
+
+        ShowHP();
+    }
+
+    /// <summary>
+    /// HPをUI表示する
+    /// </summary>
+    void ShowHP()
+    {
+        _hpText.text = "HP:" + _hp.ToString();
+    }
+}
