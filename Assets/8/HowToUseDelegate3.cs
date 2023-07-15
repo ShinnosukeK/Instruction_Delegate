1cbfd61251e35beab7ed55d99f15caf8e51d2e0e
Shinnosuke Kitayama <k.shinnosuke0603@gmail.com>
2023-07-15 11:45:24 +0900

    Answer8

--- Assets/8/HowToUseDelegate3.cs
+++ Assets/8/HowToUseDelegate3.cs
@@ -14,7 +14,7 @@ public class HowToUseDelegate3 : MonoBehaviour
     // Step5: delegateの定義やイベント処理を「実行」する役割をもっているJumpスクリプトを
     // ここで定義。変数名はjumpとしてシリアライズ化し、Jumpスクリプトがアタッチされている
     // Playerをインスペクタ上で指定しよう。
-    
+    [SerializeField] Jump jump;
 
     void JumpEvent1() {
         Debug.Log("Event1");
@@ -26,16 +26,23 @@ public class HowToUseDelegate3 : MonoBehaviour
         // Step6: jump変数がもつOnJumpedに対して、JumpEvent1とJumpEvent2を登録(+=)
         // ここまでで一旦実行し、スペースキーが押されるたびにEvent1, Event2が
         // Console上で表示されることを確認しましょう
-        
+        jump.OnJumped += JumpEvent1;
+        jump.OnJumped += JumpEvent2;
 
         // Step7: OnJumpedからJumpEvent1を解除(-=)
         // これにより実行すると、Event2のみしか実行されないことを確認してください
-        
+        jump.OnJumped -= JumpEvent1;
 
         // Step8: 1,2,1を登録。表示はどうなると予想しますか？
-        
+        jump.OnJumped += JumpEvent1;
+        jump.OnJumped += JumpEvent2;
+        jump.OnJumped += JumpEvent1;
+        // ここまでで、2121の順に逐次実行されることがわかるはず。
+        // こうして、同じイベントを複数登録することもできます。
 
         // Step9: 2を解除。表示はどうなると予想しますか？
-        
+        jump.OnJumped -= JumpEvent2;
+        // ここまでで、211の順に逐次実行されることがわかるはず。
+        // 121にならないということは、後から登録されたものから削除されていることがわかります。
     }
 }
--- Assets/8/Jump.cs
+++ Assets/8/Jump.cs
@@ -12,19 +12,19 @@ public class Jump : MonoBehaviour
 {
     // Step1: 【delegateの定義】戻り値なし、引数なしのdelegate JumpHandlerを宣言
     // ここで、Unityのイベント関連の文脈では...Handlerという名前になるのが普通
-    
+    public delegate void JumpHandler();
 
     // Step2: Step1で定義したJumpHandler型の変数OnJumpedを宣言
     // ここで、「...のイベントが発生したときに処理すること」
     //（ここではスペースキー押下時のイベントになる）
     // 的な意味なので、On...という変数名になるのが普通
-    
+    public JumpHandler OnJumped;
 
     void Update() {
         // Step3: スペースキーが押されたら
-        if() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
             // Step4: OnJumpedをInvokeする
-            
-        }    
+            OnJumped?.Invoke();
+        }
     }
 }
