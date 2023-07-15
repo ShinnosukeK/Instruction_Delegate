74e3de8a900fe7b66e91207866750dcc0c05b987
Shinnosuke Kitayama <k.shinnosuke0603@gmail.com>
2023-07-15 10:32:03 +0900

    Answer6

--- Assets/6/HowToUseDelegate1.cs
+++ Assets/6/HowToUseDelegate1.cs
@@ -5,29 +5,33 @@ using UnityEngine;
 public class HowToUseDelegate1 : MonoBehaviour
 {
     // Step1: 戻り値bool型、引数int型numのdelegate Checkerを定義
-    
+    public delegate bool Checker(int num);
 
     // Step2: 戻り値なし、引数を①delegate Checker型、②int型numを取れるCheck関数を定義
-    void Check(ここに追記) {
+    void Check(Checker checker, int num) {
         // Step3: if文条件判定に、Invokeにnumを与えて利用する
         // 通過したら「引数(num）OK」ダメなら「引数(num)NG」と出力
-        
+        if((bool)checker?.Invoke(num)) {
+            Debug.Log("引数" + num + "OK");
+        } else {
+            Debug.Log("引数" + num + "NG");
+        }
     }
 
     void Start() {
         int hp = 5000;
 
         // Step4: Checker型checkerを定義し、CanDefeatZakoを代入
-        
+        Checker checker = CanDefeatZako;
 
         // Step5: Check関数に、checkerとhpを与えて呼ぶ
-        
+        Check(checker, hp);
 
         // Step6: checkerにCanDefeatBossを再代入
-        
+        checker = CanDefeatBoss;
 
         // Step7: Check関数に、checkerとhpを与えて呼ぶ
-        
+        Check(checker, hp);
     }
 
     //前の問題で作った、intを引数としてboolを返す関数
